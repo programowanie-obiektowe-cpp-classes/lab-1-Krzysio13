3385ecb73e5793077cfab38780691a6e847c77f1
Krzysio13 <01171657@pw.edu.pl>
2024-10-27 17:24:44 +0100

    Wersja pierwsza.

--- Wektor2D.hpp
+++ Wektor2D.hpp
@@ -1,4 +1,43 @@
 class Wektor2D
 {
-    // Tutaj napisz implementacje klasy Wektor2D
+    public:
+    Wektor2D(double x=0, double y=0){
+        _x = x;
+        _y = y;
+    }
+
+    ~Wektor2D(){}
+
+    void setX(double x){
+        _x = x;
+    }
+
+    void setY(double y){
+        _y = y;
+    }
+
+    double getX(){
+        return _x;
+    }
+
+    double getY(){
+        return _y;
+    }
+
+    private:
+    double _x;
+    double _y;
 };
+
+Wektor2D operator+(Wektor2D v1, Wektor2D v2){
+    Wektor2D sum;
+    sum.setX(v1.getX() + v2.getX());
+    sum.setY(v1.getY() + v2.getY());
+    return sum;
+}
+
+double operator*(Wektor2D v1, Wektor2D v2){
+    double prod;
+    prod = (v1.getY() * v2.getY()) + (v1.getX() * v2.getX());
+    return prod;
+}
